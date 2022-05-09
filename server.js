2234719c899ad0f5d776d40d8388f605285bddd2
alisondraV <alice.7414122013@gmail.com>
2022-05-09 16:04:32 -0400

    Move express request handlers into the MongoClient's call

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+MongoClient.connect(process.env.MONGODB_CONNECTION)
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('starwars')
 
-const connectionString = process.env.MONGODB_CONNECTION;
+        app.use(bodyParser.urlencoded({ extended: true }))
 
-MongoClient.connect(connectionString,(err, client) => {
-    if (err) return console.error(err)
-    console.log('Connected to Database')
-})
+        app.listen(3000, function() {
+            console.log('listening on 3000')
+        })
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
-})
+        app.get('/', (req, res) => {
+            res.sendFile(__dirname+'/index.html')
+        })
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname+'/index.html')
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-})
+        app.post('/quotes', (req, res) => {
+            console.log(req.body)
+        })
+    })
+    .catch(error => console.error(error))

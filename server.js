c579f8b19982b6aeae5f67143782e639aba387f0
alisondraV <alice.7414122013@gmail.com>
2022-05-09 16:57:06 -0400

    Get list of quotes

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express')
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
+app.set('view engine', 'ejs')
 
 MongoClient.connect(process.env.MONGODB_CONNECTION)
     .then(client => {
         console.log('Connected to Database')
         const db = client.db('starwars')
+        const quotesCollection = db.collection('quotes')
 
         app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -16,11 +18,20 @@ MongoClient.connect(process.env.MONGODB_CONNECTION)
         })
 
         app.get('/', (req, res) => {
-            res.sendFile(__dirname+'/index.html')
+            db.collection('quotes').find().toArray()
+                .then(results => {
+                    res.render('index.ejs', { quotes: results })
+                })
+                .catch(error => console.error(error))
         })
 
         app.post('/quotes', (req, res) => {
-            console.log(req.body)
+            quotesCollection.insertOne(req.body)
+                .then(result => {
+                    console.log(result)
+                    res.redirect('/')
+                })
+                .catch(error => console.error(error))
         })
     })
     .catch(error => console.error(error))

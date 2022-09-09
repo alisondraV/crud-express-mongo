e58b0e4a5089f173c23c58c881b3cd34b43afbe8
alisondraV <alice.7414122013@gmail.com>
2022-09-09 15:55:37 -0400

    Fix the delete and update functionality

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ config()
 
 const app = express()
 app.set('view engine', 'ejs')
-app.use('/public', express.static('public'));
+app.use(express.static('public'))
 app.use(bodyParser.json())
 
 ;(async () => {
